a6fc137d97765bad973487cbda93bbc10c9ed29f
Vitor Sandes <sandesvitor@gmail.com>
2020-05-08 20:21:44 -0300

    Create New Py File - miner for PDFMINER

--- seletorCenas.py
+++ seletorCenas.py
@@ -1,1 +1,17 @@
-print('Hello World')
+screenPlay = open("C:\\Users\\Snades\\Desktop\\roteiros\\roteiro1.txt", "r")
+
+
+scene_bool_1 = "EXT"
+scene_bool_2 = "INT"
+lines = []
+
+    
+for block in screenPlay:
+    line = block.split('\n')
+    lines.append(line)
+
+for x in lines:
+    if x.__contains__('EXT'):
+        print(x)
+
+
