6ca227a85ad93c3374097219a8fe99f494145654
Vitor Sandes <sandesvitor@gmail.com>
2020-05-09 14:51:37 -0300

    New file interface.py updated

--- interface.py
+++ interface.py
@@ -1,2 +1,10 @@
 from tkinter import *
 from resources.scriptMiner import ScriptMiner
+
+class Application:
+    def __init__(self, master=None):
+        pass
+
+root = Tk()
+Application(root)
+root.mainloop()
