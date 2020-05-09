dd90a30d8046115fefce3d7f773cb22087e03383
Vitor Sandes <sandesvitor@gmail.com>
2020-05-09 12:31:59 -0300

    Create module miner and __init__.py, main file is seletorCenas

--- resources/miner.py
+++ resources/miner.py
@@ -7,65 +7,75 @@ from pdfminer.layout import LAParams
 from pdfminer.pdfpage import PDFPage
 
 
-def convert_pdf_to_txt(path):
-    rsrcmgr = PDFResourceManager()
-    retstr = io.StringIO()
-    codec = 'utf-8'
-    laparams = LAParams()
-    device = TextConverter(rsrcmgr, retstr, codec=codec, laparams=laparams)
-    fp = open(path, 'rb')
-    interpreter = PDFPageInterpreter(rsrcmgr, device)
-    password = ""
-    maxpages = 0
-    caching = True
-    pagenos = set()
+class ScriptMiner(PDFPageInterpreter, PDFResourceManager, TextConverter,LAParams,PDFPage):
+    def __init__(self, path):
+        self.path = path
+        #super.__init__()
 
-    for page in PDFPage.get_pages(fp, pagenos, maxpages=maxpages, password=password, caching=caching, check_extractable=True):
-        interpreter.process_page(page)
+    def convert_pdf_to_txt(self):
+        rsrcmgr = PDFResourceManager()
+        retstr = io.StringIO()
+        codec = 'utf-8'
+        laparams = LAParams()
+        device = TextConverter(rsrcmgr, retstr, codec=codec, laparams=laparams)
+        fp = open(self.path, 'rb')
+        interpreter = PDFPageInterpreter(rsrcmgr, device)
+        password = ""
+        maxpages = 0
+        caching = True
+        pagenos = set()
 
-
-
-    fp.close()
-    device.close()
-    text = retstr.getvalue()
-    retstr.close()
-    return text
+        for page in PDFPage.get_pages(fp, pagenos, maxpages=maxpages, password=password, caching=caching, check_extractable=True):
+            interpreter.process_page(page)
 
 
-def find_between(s, first, last):
-    try:
-        start = s.index(first) + len(first)
-        end = s.index(last, start)
-        return s[start:end]
-    except ValueError:
-        return ""
+        fp.close()
+        device.close()
+        text = retstr.getvalue()
+        retstr.close()
+        return text
+
+
+    def scenesList(self):
+        screenPlay = self.convert_pdf_to_txt()
+    
+        block = screenPlay.split('\n')
+        scenes_header = []
+        scenes = []
+
+        for line in block:
+            if 'EXT.' in line:
+                scenes_header.append(line)
+            elif 'INT.' in line:
+                scenes_header.append(line)
         
+        for i in range(len(scenes_header) - 1):
+            try:
+                start = screenPlay.index(scenes_header[i]) + len(scenes_header[i])
+                end = screenPlay.index(scenes_header[i + 1], start)
+                s = screenPlay[start:end]
+
+                scene = scenes_header[i] + '\n' + s
+                scenes.append(scene)                
+            except ValueError:
+                scenes.append("")
+        
+        return scenes
 
 
-path_pdf = 'C:\\Users\\Snades\\Desktop\\roteiros\\roteiro1.pdf'
+    def scenesHeaderNumbered(self): #precisa tratar os espaços em branco nas headers!
+        screenPlay = self.convert_pdf_to_txt()
+    
+        block = screenPlay.split('\n')
+        scenes_header_numbered = []
+        count = 1
 
-screenPlay = convert_pdf_to_txt(path_pdf)
-block = screenPlay.split('\n')
-scenes_header = []
-scenes_header_numbered = [] #tem o número da cena antes ---> para organizar depois no front-end!
-scenes = []
-count = 1
+        for line in block:
+            if 'EXT.' in line:
+                scenes_header_numbered.append(f'Cena {count} - ' + line)
+                count += 1
+            elif 'INT.' in line:
+                scenes_header_numbered.append(f'Cena {count} - ' + line)
+                count += 1
 
-for line in block:
-    if 'EXT.' in line:
-        scenes_header.append(line)
-        scenes_header_numbered.append(f'Cena {count} - ' + line)
-        count += 1
-    elif 'INT.' in line:
-        scenes_header.append(line)
-        scenes_header_numbered.append(f'Cena {count} - ' + line)
-        count += 1
-
-
-for i in range(len(scenes_header) - 1):
-    scene = find_between(screenPlay, scenes_header[i], scenes_header[i+1])
-    scene = scenes_header[i] + '\n' + scene
-    scenes.append(scene)
-
-print(scenes[0])
-
+        return scenes_header_numbered
