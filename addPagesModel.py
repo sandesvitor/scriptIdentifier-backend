3242adb80ead227dcdad298deffeb06bae19a0d4
Vitor Sandes <sandesvitor@gmail.com>
2020-05-10 00:28:11 -0300

    Create new file for modelling page addition

---
+++ addPagesModel.py
@@ -0,0 +1,70 @@
+import tkinter as tk
+
+
+LARGE_FONT=("Verdana, 12")
+
+class Application(tk.Tk):
+    def __init__(self, *args, **kwargs):
+        tk.Tk.__init__(self, *args, **kwargs)
+        
+        container1 = tk.Frame(self)
+        container1.pack(side="top", fill="both", expand=True) 
+        container1.grid_rowconfigure(0, weight=1)
+        container1.grid_columnconfigure(0, weight=1)
+
+        self.frames = {}
+
+        for F in (StartPage, PageOne, PageTwo): #always add the new page class to this loop!
+        
+            frame = F(container1, self)
+            self.frames[F] = frame
+            frame.grid(row=0, column=0, sticky="nsew")
+
+        self.show_frame(StartPage)
+    
+    def show_frame(self, cont):
+        frame = self.frames[cont]
+        frame.tkraise()
+
+
+class StartPage(tk.Frame):
+    def __init__(self,parent, controller):
+        tk.Frame.__init__(self, parent)
+        label = tk.Label(self, text="Start Page", font=LARGE_FONT)
+        label.pack(pady=10, padx=10)
+
+        button1 = tk.Button(self, text="Visit Page 1", command=lambda: controller.show_frame(PageOne))
+        button1.pack()
+
+        button2 = tk.Button(self, text="Visit Page 2", command=lambda: controller.show_frame(PageTwo))
+        button2.pack()
+
+
+class PageOne(tk.Frame):
+    def __init__(self, parent,controller):
+        tk.Frame.__init__(self, parent)
+        label = tk.Label(self, text="Page One", font=LARGE_FONT)
+        label.pack(pady=10, padx=10)
+
+        button1 = tk.Button(self, text="Back to Home", command=lambda: controller.show_frame(StartPage))
+        button1.pack()
+
+        button2 = tk.Button(self, text="Page Two", command=lambda: controller.show_frame(PageTwo))
+        button2.pack()
+
+
+class PageTwo(tk.Frame):
+    def __init__(self, parent,controller):
+        tk.Frame.__init__(self, parent)
+        label = tk.Label(self, text="Page Two", font=LARGE_FONT)
+        label.pack(pady=10, padx=10)
+
+        button1 = tk.Button(self, text="Back to Home", command=lambda: controller.show_frame(StartPage))
+        button1.pack()
+
+        button2 = tk.Button(self, text="Page One", command=lambda: controller.show_frame(PageOne))
+        button1.pack()
+      
+
+app = Application()
+app.mainloop()
--- interfacetTest.py
+++ interfacetTest.py
@@ -1,17 +1,24 @@
 from tkinter import *
+import tkinter
+from tkinter import messagebox
 from resources.scriptMiner import ScriptMiner
 
-class Application:
+
+class Application():
     def __init__(self, master=None):
         self.fonte = ("Verdana", "12")
 
         self.container1 = Frame(master)
         self.container1["pady"] = 20
-        self.container1.pack() 
-        
+        self.container1.pack(side="top", fill="both", expand=True) 
+        self.container1.grid_rowconfigure(0, weight=1)
+        self.container1.grid_columnconfigure(0, weight=1)
+
         self.container2 = Frame(master)
         self.container2["pady"] = 30
-        self.container2.pack() 
+        self.container2.pack()
+        self.container2.grid_rowconfigure(0, weight=1)
+        self.container2.grid_columnconfigure(0, weight=1) 
 
         self.lblheader = Label(self.container1, text="Lets get started!", font=self.fonte, width=50) 
         self.lblheader.pack()
@@ -19,10 +26,10 @@ class Application:
         self.btnUpload = Button(self.container2, text="Upload script PDF", font=self.fonte, width=20)
         self.btnUpload["command"] = self.getFile_nextStep
         self.btnUpload.pack(side=LEFT)
-    
+
+        
     def getFile_nextStep(self):
-        pass
-       
+        tkinter.messagebox.showinfo("Debugging", "Tudo certo até aqui.")    
         
 
 root = Tk()
